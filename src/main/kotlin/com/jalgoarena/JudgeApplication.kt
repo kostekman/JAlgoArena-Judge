363a4705b3747b1c881a0b06665f3d134968a7d6
Adam Koscinski <kostekman1@gmail.com>
2018-01-16 19:09:41 +0100

    Adding swagger integration.

--- src/main/kotlin/com/jalgoarena/JudgeApplication.kt
+++ src/main/kotlin/com/jalgoarena/JudgeApplication.kt
@@ -4,15 +4,31 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 @EnableCaching
 open class JudgeApplication {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(JudgeApplication::class.java, *args)
         }
+    }
+
+    @Bean
+    open fun judgeApi(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
     }
 }
 
